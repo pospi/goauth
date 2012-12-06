fe61ed03af773cfb0ae388d28523c8e702b4d228
pospi <pospi@spadgos.com>
2012-12-06 11:47:28 +1000

    implemented state awareness onto Flow class by way of sessions

--- flow.class.php
+++ flow.class.php
@@ -14,11 +14,56 @@ class GOAuthFlow implements ArrayAccess, Iterable
 {
 	private $actions = array();
 
+	const SESSION_PROGRESS_KEY	= 'goauth_progress';
+	const SESSION_STATE_KEY		= 'goauth_state';
+
+	/**
+	 * Gets the current progress of the flow (index we are up to in $this->actions)
+	 *
+	 * :WARNING: if your own code is responsible for initializing sessions, be certain that you don't call this method
+	 * 			 before doing so as this method will begin a session if one is not already present.
+	 *
+	 * @return scalar
+	 */
+	public function getCurrentProgress()
+	{
+		self::ensureSession();
+		return isset($_SESSION[GOAuthFlow::SESSION_PROGRESS_KEY]) ? $_SESSION[GOAuthFlow::SESSION_PROGRESS_KEY] : null;
+	}
+
+	/**
+	 * Executes the flow, picking up where we left off.
+	 *
+	 * :WARNING: if your own code is responsible for initializing sessions, be certain that you don't call this method
+	 * 			 before doing so as this method will begin a session if one is not already present.
+	 *
+	 * @param  array  $params initial parameters to pass for beginning the flow.
+	 */
 	public function execute($params = array())
 	{
-		foreach ($this->actions as $action) {
+		$startAt = $this->getCurrentProgress();
+		$started = false;
+
+		foreach ($this->actions as $i => $action) {
+			if ($startAt !== null && !$started) {
+				if ($action == $startAt) {
+					$started = true;
+				}
+				continue;
+			}
+
 			$action->setParams($params);		// pass parameters to first action
 			$params = $action->process();		// use the return value as input to the next action in the flow
+
+			// if this was a terminal action, jump out
+			if ($action->isFinal()) {
+
+				// store the progress we're at in the flow in a session so we can pick it up easily later
+				self::ensureSession();
+				$_SESSION[self::SESSION_PROGRESS_KEY] = $i;
+
+				return $params;
+			}
 		}
 		return $params;		// return the result from the terminal action for further processing
 	}
@@ -41,6 +86,7 @@ class GOAuthFlow implements ArrayAccess, Iterable
 		} else {
 			$this->actions[$k] = $v;
 		}
+
 		return true;
 	}
 
