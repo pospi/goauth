26c4e224f309957c27cace6f3407a06e6a8e6cb6
pospi <pospi@spadgos.com>
2012-12-11 18:47:12 +1000

    better handling of meta redirects and ability to configure delayed ones

--- action_redirect.class.php
+++ action_redirect.class.php
@@ -5,8 +5,10 @@
  * Parameters:
  * 	uri		- URL to redirect the remote user to
  * 	get		- any GET parameters to pass with the redirect
+ * 	delay	- perform an instant (header) redirect with 0, or a delayed meta redirect. Default is 0, however note that most OAuth services will not work
+ * 			  if you initiate a request flow as a header redirect.
  * 	page_content	- a string, Response object or array of output chunks to echo for the page upon redirecting.
- * 					  In good practise, this should contain a <meta http-equiv="refresh"> tag to redirec the user agent, as well as a link to the new URL.
+ * 					  In good practise, this should contain a link to the new URL. No meta http-refresh tag is necessary, as this is output as part of the action.
  * 					  note that this will only be visible to users running browsers which do not obey header redirects.
  *
  * @package	goauth
@@ -15,7 +17,9 @@
  */
 class GOAuthAction_Redirect extends GOAuthAction
 {
-	protected $params = array('page_content' => 'You are now being redirected to <a href="%1$s">%1$s</a> <meta http-equiv="refresh" content="0;URL=\'%1$s\'">');
+	protected $params = array('page_content' => 'You are now being redirected to <a href="%1$s">%1$s</a>');
+
+	const META_TAG = '<meta http-equiv="refresh" content="%1$d;URL=\'%2$s\'">';
 
 	/**
 	 * Process the redirect action. The script continues after this point -
@@ -32,7 +36,7 @@ class GOAuthAction_Redirect extends GOAuthAction
 		}
 
 		// check for headers sent, and log error if we can't redirect
-		if (headers_sent()) {
+		if (headers_sent() && empty($this->params['delay'])) {
 			if ($this->debug) {
 				$this->debug[] = "Unable to redirect to $url: headers already sent";
 			}
@@ -44,12 +48,19 @@ class GOAuthAction_Redirect extends GOAuthAction
 			$this->debug[] = "Redirecting to " . $url;
 		}
 
+		$resp = new Response();
+		$resp->addBlock(sprintf(self::META_TAG, isset($this->params['delay']) ? $this->params['delay'] : 0, $url));
+
 		if (is_string($this->params['page_content'])) {
 			$this->params['page_content'] = sprintf($this->params['page_content'], $url);
 		}
-		$resp = new Response();
+
 		$resp->addBlock($this->params['page_content']);
-		$resp->redirect($url);
+		if (empty($this->params['delay'])) {
+			$resp->redirect($url);
+		} else {
+			$resp->send();
+		}
 
 		return true;
 	}
