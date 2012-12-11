41d72fb4157a04643cde6c21295d836111c5f562
pospi <pospi@spadgos.com>
2012-12-11 17:57:04 +1000

    allow configuring page content to echo in redirect action

--- action_redirect.class.php
+++ action_redirect.class.php
@@ -5,6 +5,9 @@
  * Parameters:
  * 	uri		- URL to redirect the remote user to
  * 	get		- any GET parameters to pass with the redirect
+ * 	page_content	- a string, Response object or array of output chunks to echo for the page upon redirecting.
+ * 					  In good practise, this should contain a <meta http-equiv="refresh"> tag to redirec the user agent, as well as a link to the new URL.
+ * 					  note that this will only be visible to users running browsers which do not obey header redirects.
  *
  * @package	goauth
  * @author	Sam Pospischil <pospi@spadgos.com>
@@ -12,6 +15,8 @@
  */
 class GOAuthAction_Redirect extends GOAuthAction
 {
+	protected $params = array('page_content' => 'You are now being redirected to <a href="%1$s">%1$s</a> <meta http-equiv="refresh" content="0;URL=\'%1$s\'">');
+
 	/**
 	 * Process the redirect action. The script continues after this point -
 	 * you may perform any additional logic before terminating the script at your discretion.
@@ -34,12 +39,16 @@ class GOAuthAction_Redirect extends GOAuthAction
 			return false;
 		}
 
-		// send the redirect header
+		// send the redirect header & body
 		if ($this->debug) {
 			$this->debug[] = "Redirecting to " . $url;
 		}
 
+		if (is_string($this->params['page_content'])) {
+			$this->params['page_content'] = sprintf($this->params['page_content'], $url);
+		}
 		$resp = new Response();
+		$resp->addBlock($this->params['page_content']);
 		$resp->redirect($url);
 
 		return true;
